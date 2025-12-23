6e1beb8f447b0d41e956b1bb3c1407e13fce0e0e
Arpspractice <arpithas1121@gmail.com>
2025-12-23 16:40:41 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "cluster_name" {
   description = "EKS cluster name"
   type        = string
-  default     = "demo-eks-cluster"
+  default     = "webapp-eks-cluster"
 }
 
 variable "vpc_cidr" {
